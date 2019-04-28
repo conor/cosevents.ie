77aa4c5534fc96a51f4249cf0a3cf7b87090975c
Conor Wade <conorwade@gmail.com>
2019-04-28 20:58:03 +0800

    Add favicons

--- components/Header.js
+++ components/Header.js
@@ -20,6 +20,34 @@ const Header = () => (
       <title>
         COSevents.ie - Stress free event planning based out of Limerick, Ireland
       </title>
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/static/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/static/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/static/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/static/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/static/safari-pinned-tab.svg"
+        color="#d8328b"
+      />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#4f354a" />
+      <meta name="msapplication-config" content="/static/browserconfig.xml" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
     <a href="/">
       <Logo />
